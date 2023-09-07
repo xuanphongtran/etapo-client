c048d795ff180bd4bc918e418630f3818e4c1e3e
phongbvb <phongbvb.09@gmail.com>
2023-09-07 17:06:27 +0700

    fix eslint

--- pages/products.js
+++ pages/products.js
@@ -3,14 +3,14 @@ import Center from '@/components/Center'
 import { mongooseConnect } from '@/lib/mongoose'
 import { Product } from '@/models/Product'
 import ProductsGrid from '@/components/ProductsGrid'
-import Title from '@/components/Title'
+// import Title from '@/components/Title'
 
 export default function ProductsPage({ products }) {
   return (
     <>
       <Header />
       <Center>
-        <Title>All products</Title>
+        {/* <Title>All products</Title> */}
         <ProductsGrid products={products} />
       </Center>
     </>
